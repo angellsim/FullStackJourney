49b09afefb75857afadd9e6ef71ccd9215b54722
raiden <alicewolffernandes@gmail.com>
2026-01-22 15:14:33 -0300

    Variables with var

---
+++ javascript/exercises/ex002/script.js
@@ -0,0 +1,17 @@
+// Declarando uma variável sem valor atribuído
+var nome
+
+console.log(nome)
+
+// Declarando uma variável com valor atribuído
+
+var email = "alice@gmail.com"
+
+console.log(email)
+
+// Alterando o valor de uma variável
+
+email = "anapaula@gmail.com"
+
+console.log(email)
+
