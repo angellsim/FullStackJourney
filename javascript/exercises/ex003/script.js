b6bb2140217416056d61b6444061616853bcfeff
raiden <alicewolffernandes@gmail.com>
2026-01-22 15:21:17 -0300

    Let and Const variables

---
+++ javascript/exercises/ex003/script.js
@@ -0,0 +1,19 @@
+// Pela primeira vez que se cria a variável, ela é definida normalmente
+let nome
+
+console.log(nome)
+
+// Porém, ao tentar criar ela novamente (usando let) ela dará um erro no sistema
+//let nome = 'João' // Erro: Identifier 'nome' has already been declared
+
+/* Outra possibilidade de declarar variáveis é utilizando o termo CONST
+Ele é utilizado quando não se pretende alterar o valor da variável
+*/
+
+const nomeCompleto = 'Satoru Gojo'
+
+console.log(nomeCompleto)
+
+// Caso tente alterar o valor desta variável será retornado um erro
+
+
