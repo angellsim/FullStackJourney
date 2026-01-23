5b5ea2b0f6b410fe9d87962831cb4b058b249438
raiden <alicewolffernandes@gmail.com>
2026-01-22 23:47:26 -0300

    Strings explained better

---
+++ javascript/exercises/ex005/script.js
@@ -0,0 +1,22 @@
+let nome = "Gojo" 
+console.log("Olá, " + nome + "!")
+console.log(typeof nome)
+
+console.log("As Strings podem ser declaradas tanto com aspas duplas quanto com aspas simples.")
+console.log('Viu só?!')
+
+// A diferença entre elas é poder utilizar as aspas ao parafrasear algum conteúdo
+
+console.log("Com aspas duplas você pode envelopar a frase com 'aspas simples'")
+console.log('E com aspas simples você pode envelopar a frase com "aspas duplas"')
+
+// Também é possível escrever String utilizando crases (` `) Elas permitem que o usuário escreva linhas com formatação, múltiplas linhas e etc.
+
+console.log(`
+  Neste exemplo, você pode escrever pulando
+  espaços ou apenas seguindo o fluxo normalmente aqui ele
+  respeita
+  da
+  maneira que for
+  escrita
+  `)
