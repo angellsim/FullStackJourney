76025bfb6a6005e13c60be0876fb59a79050589f
raiden <alicewolffernandes@gmail.com>
2026-01-24 15:08:58 -0300

    Introduction to numbers

---
+++ javascript/exercises/ex007/script.js
@@ -0,0 +1,17 @@
+// Int number
+
+let number = 3
+
+// Int number that could be negative as well
+
+number = 4
+
+// Float number
+
+number = 4.5
+
+// Float number that could be negative as well
+
+number = -4.5
+
+// Operators --> * / - + % 
