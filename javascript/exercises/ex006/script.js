6fdffe506c4d2d69862536bc42a3f1ee0fb05902
raiden <alicewolffernandes@gmail.com>
2026-01-24 15:01:00 -0300

    Template literals and string concatenation

---
+++ javascript/exercises/ex006/script.js
@@ -0,0 +1,16 @@
+let mainUsername = "Gojo Satoru"
+let otherUsername = "Geto Suguru"
+
+// Template Literals
+console.log(mainUsername, otherUsername)
+
+//String Concatenation
+console.log(mainUsername + " <3 " + otherUsername)
+
+let msg = mainUsername + " loves " + otherUsername + " so much!"
+
+console.log(msg)
+
+// Using template literals for better understandability
+console.log(`${mainUsername} loves ${otherUsername} so much it's insane!`)
+
