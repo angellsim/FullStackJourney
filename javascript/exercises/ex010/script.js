aedb89810ec0687024e52918aec92bf32bd19fe4
raiden <alicewolffernandes@gmail.com>
2026-01-25 02:40:02 -0300

    E, ou e negação

---
+++ javascript/exercises/ex010/script.js
@@ -0,0 +1,14 @@
+email = true
+senha = true
+
+// E && 
+
+console.log(email && senha) // Resultado = true (já que os dois valores são verdadeiros)
+
+// Ou || 
+
+console.log(email || senha) // Resultado = true (já que pelo menos um dos valores é verdadeiro)
+
+// Negação ! 
+
+console.log(!email) // Resultado = false (negação do valor verdadeiro de email)
