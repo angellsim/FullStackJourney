fb02d5d632ef3627e02632b36e9774fc9009b225
raiden <alicewolffernandes@gmail.com>
2026-01-25 18:08:21 -0300

    Switch case

---
+++ javascript/exercises/ex013/script.js
@@ -0,0 +1,15 @@
+let option = 1
+
+switch (option) {
+  case 1:
+    console.log("A opção é 1")
+    break
+  case 2:
+    console.log("A opção é 2")
+    break
+  case 3:
+    console.log("A opção é 3")
+    break
+}
+
+// É importante sempre ter um break logo após o comando do switch-case porque se não o código executará todos os cases posteriores à ele. 
