721218f6c19270b6607f7677d5f008b5282326ba
raiden <alicewolffernandes@gmail.com>
2026-01-25 03:35:51 -0300

    If, else if and else

--- javascript/exercises/ex012/script.js
+++ javascript/exercises/ex012/script.js
@@ -8,4 +8,8 @@ let idade = 27
 
 if (idade != 28) {
   console.log(`You are not ${nome}!`)
+} else if (idade > 30) {
+  console.log("You are older than 30!")
+} else { 
+  console.log("You are Satoru Gojo! Probably...")
 }
