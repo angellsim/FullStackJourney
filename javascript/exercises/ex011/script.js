8d7fce38c1484eb8cd74bf476ec2bf01e36a2865
raiden <alicewolffernandes@gmail.com>
2026-01-25 03:04:43 -0300

    Operador condicional ternário

---
+++ javascript/exercises/ex011/script.js
@@ -0,0 +1,7 @@
+let age = 16
+
+console.log(age >= 18 
+  ? "Adult" 
+  : "Minor")
+
+  // É basicamente um if simplificado. O ponto de interrogação significa "se sim" e os dois pontos significa "se não"
