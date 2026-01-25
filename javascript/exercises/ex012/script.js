b9df2026298c6806a765e77935371252cff2b7d0
raiden <alicewolffernandes@gmail.com>
2026-01-25 03:18:59 -0300

    If statement

---
+++ javascript/exercises/ex012/script.js
@@ -0,0 +1,11 @@
+let nome = "Satoru Gojo"
+
+if (nome == "Satoru Gojo") {
+  console.log("You are the strongest jujutsu sorcerer alive!")
+}
+
+let idade = 27
+
+if (idade != 28) {
+  console.log(`You are not ${nome}!`)
+}
