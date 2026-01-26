81d9bb0580e7aa30a79aa21dfc31ae13cb69329e
raiden <alicewolffernandes@gmail.com>
2026-01-26 15:37:14 -0300

    Arrow function

---
+++ javascript/exercises/ex020/script.js
@@ -0,0 +1,12 @@
+const showMessage1 = () => {
+  console.log("Olá")
+}
+
+console.log(showMessage1)
+showMessage1()
+
+const showMessage2 = (nomePessoa) => {
+  console.log("Olá, " + nomePessoa + "!")
+}
+
+showMessage2("Maria")
