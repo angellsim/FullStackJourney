d516cc510c41d2c3a2f9862bcbc56f2f52e9041a
raiden <alicewolffernandes@gmail.com>
2026-01-26 20:34:46 -0300

    Query Selector

---
+++ javascript/exercises/ex023/script.js
@@ -0,0 +1,9 @@
+// Query Selector é como se fosse o seletor do CSS. Ele retorna o primeiro elemento que encontrar dentro da especificidade escolhida. 
+
+// Também temos o Query Seletor All que é a mesma coisa porém ele retornará todos os valores que estiverem contidos dentro do elemento.
+
+// Retorna apenas o primeiro elementos 
+const guest3 = document.querySelector('#guest')
+
+// Retorna todos os elementos encontrados
+const guest4 = document.querySelectorAll('.guest')
