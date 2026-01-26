93de1593aa1954cbe4e54d6e9464d12386a00abd
raiden <alicewolffernandes@gmail.com>
2026-01-26 15:25:10 -0300

    Comentário de Documentação em Funções

---
+++ javascript/exercises/ex018/script.js
@@ -0,0 +1,17 @@
+/*
+  Para adicionar comentários de documentação (que são exibidos quando o usuário passa o mouse por cima da função)
+  É necessário adicionar /** antes do comentário e adicionar os parâmetros e retorno da função.
+  É muito útil para especificar melhor o que uma função faz principalmente para trabalhar em conjunto com outras pessoas
+*/
+
+/**
+ * 
+ * @param {String} email user email. 
+ * @param {String} password more than 6 characters.
+ * @returns {number} user id.
+ */
+function signIn(email, password) {
+  // Fluxo de autenticação do usuário
+
+  return 7
+}
