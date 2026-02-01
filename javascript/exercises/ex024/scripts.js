2431f905986dec7443e0bf77fb96739fc33f8877
raiden <alicewolffernandes@gmail.com>
2026-02-01 14:29:41 -0300

    Manipulando conteúdo com JavaScript

---
+++ javascript/exercises/ex024/scripts.js
@@ -0,0 +1,7 @@
+const guest = document.querySelector('#guest-1')
+/* console.log(guest.textContent)
+guest.textContent = "João" */
+
+console.log(guest.textContent) // Retorna o conteúdo visível e oculto
+console.log(guest.innerText) // Retorna somente o conteúdo visível
+console.log(guest.innerHTML) // Retorna o conteúdo HTML
