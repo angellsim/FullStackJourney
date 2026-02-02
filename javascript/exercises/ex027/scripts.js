8d7207b32aa7e5368ca534a03193687a53534909
raiden <alicewolffernandes@gmail.com>
2026-02-02 19:33:43 -0300

    Manipulando atributos

---
+++ javascript/exercises/ex027/scripts.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("input")
+
+// Atualiza um atributo do elemento
+// Desabilita o campo de input
+// input.setAttribute("disabled", true)
+
+// Selecionar arquivo no campo de input
+// input.setAttribute("type", "file")
+
+// Remove um atributo do elemento
+//input.removeAttribute("id")
