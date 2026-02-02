0cd9d114bac4b1976de47fb6fbb2b4e859fc425d
raiden <alicewolffernandes@gmail.com>
2026-02-02 18:38:59 -0300

    Alterando Estilos

---
+++ javascript/exercises/ex025/scripts.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('#name')
+
+// Adiciona uma classe para variável
+// input.classList.add("input-error")
+
+// Remove a classe
+// input.classList.remove("input-error")
+
+// Se a classe tiver o "input-error" ele REMOVE e se não tiver ele ADICIONA
+// input.classList.toggle("input-error")
+
+const button = document.querySelector('button')
+
+// Modificar as propriedades CSS de um elemento
+button.style.backgroundColor = 'red'
