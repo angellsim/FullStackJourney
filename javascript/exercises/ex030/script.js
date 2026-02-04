91e0cca562c2f046423e5e270ff24f25926d40ec
raiden <alicewolffernandes@gmail.com>
2026-02-04 18:45:28 -0300

    Eventos de Formulário

--- javascript/exercises/ex030/script.js
+++ javascript/exercises/ex030/script.js
@@ -2,6 +2,8 @@ let form = document.querySelector("form");
 
 // A diferença entre usar o addEventListener ou ir diretamente é que diretamente ele irá ler somente a última função atribuída ao evento, enquanto utilizar o AddEventListener permite adicionar múltiplas funções que serão executadas igualmente.
 
+// O evento "submit" é específico para elementos de formulário (form). Ele é disparado quando o formulário é enviado, seja por meio de um botão de envio ou pressionando a tecla Enter em um campo de entrada dentro do formulário. Outros elementos HTML não possuem o evento "submit".
+
 form.onsubmit = (event) => {
   event.preventDefault();
   console.log("Você enviou o formulário #1")
