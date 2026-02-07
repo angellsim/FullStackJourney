b95741e00c0666d4ec41eeec429aeeb23366b60d
raiden <alicewolffernandes@gmail.com>
2026-02-07 18:39:34 -0300

    Convert Currency function added

--- projects/Convert/src/script.js
+++ projects/Convert/src/script.js
@@ -2,6 +2,10 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 
+const USD = "5.25"
+const EUR = "5.00"
+const GBP = "6.00"
+
 amount.addEventListener("input", () => {
   
   const hasCharacterRegex = /\D+/g
@@ -12,5 +16,20 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log(currency.value)
+  switch (currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+function convertCurrency(amount, price, currency) {
+
+  console.log(amount / price)
 }
