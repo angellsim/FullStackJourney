51382f4a9a3077a347e8f602861978ca7f0af393
raiden <alicewolffernandes@gmail.com>
2026-02-07 16:30:31 -0300

    Added regex to input

--- projects/Convert/src/script.js
+++ projects/Convert/src/script.js
@@ -1,5 +1,8 @@
 const amount = document.getElementById("amount")
 
 amount.addEventListener("input", () => {
-  console.log(amount.value)
+  
+  const hasCharacterRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharacterRegex, "")
+
 })
